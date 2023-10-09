6e78a3af08e430bcd2f9a62a73411321f58a7763
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-10-09 17:30:27 -0300

    Add open and close actions to drawer

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
 
     private func resetImageState() {
         return withAnimation(.spring()) {
@@ -138,12 +139,17 @@ struct ContentView: View {
 
             .overlay(
                 HStack(spacing: 12) {
-                    Image(systemName: "chevron.compact.left")
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
                         .resizable()
                         .scaledToFit()
                         .frame(height: 40)
                         .padding(8)
                         .foregroundStyle(.secondary)
+                        .onTapGesture {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        }
 
                     Spacer()
                 }
@@ -152,7 +158,8 @@ struct ContentView: View {
                     .cornerRadius(12)
                     .opacity(isAnimating ? 1 : 0)
                     .frame(width: 260)
-                    .padding(.top, UIScreen.main.bounds.height / 12),
+                    .padding(.top, UIScreen.main.bounds.height / 12)
+                    .offset(x: isDrawerOpen ? 20 : 215),
                 alignment: .topTrailing
             )
         }
