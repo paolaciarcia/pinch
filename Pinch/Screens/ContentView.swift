2efa0241b248bd1f9a30a585312af69b2cdc0dfd
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-10-09 14:58:23 -0300

    Add imageScale and imageOffset

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1.0)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    withAnimation(.spring()) {
+                                        imageScale = 1
+                                        imageOffset = .zero
+                                    }
+                                }
+                            }
+                    )
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1.0)) {
+                    isAnimating = true
+                }
+            }
         }
-        .padding()
+        .navigationViewStyle(.stack)
     }
 }
 
