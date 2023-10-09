9240704a8318e3f344dfd7de7f1050affd27cb84
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-10-09 13:54:49 -0300

    Add project

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Paola Golombieski Ciarcia on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
